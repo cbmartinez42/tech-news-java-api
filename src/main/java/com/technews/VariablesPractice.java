30b6106075ba35f0d669d0c5f526d680c1889b60
cbmartinez42 <cbmartinez42@gmail.com>
2021-11-22 15:34:34 -0600

    adding variablespractice

---
+++ src/main/java/com/technews/VariablesPractice.java
@@ -0,0 +1,15 @@
+package com.technews;
+
+public class VariablesPractice {
+    public static void main(String[] args) {
+        int myAge = 43;
+        double myAgeTwice = 43.43;
+        String dog = "Rufio";
+
+        double sum = myAge + myAgeTwice;
+        String concat = myAge + dog;
+
+        System.out.println(sum);
+        System.out.println(concat);
+    }
+}
