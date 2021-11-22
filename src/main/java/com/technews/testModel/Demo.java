d60a18bed3cb1905bb8c181d3b7c283a1da7fd76
cbmartinez42 <cbmartinez42@gmail.com>
2021-11-22 16:49:20 -0600

    adding toString

--- src/main/java/com/technews/testModel/Demo.java
+++ src/main/java/com/technews/testModel/Demo.java
@@ -1,5 +1,7 @@
 package com.technews.testModel;
 
+import java.util.Objects;
+
 public class Demo {
     private String name;
     private int age;
@@ -23,5 +25,26 @@ public class Demo {
 
     public void setAge(int age) {
         this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Demo demo = (Demo) o;
+        return age == demo.age && Objects.equals(name, demo.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+
+    @Override
+    public String toString() {
+        return "Demo{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
     }
 }
